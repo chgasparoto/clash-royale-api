38ecca6f4ac73d28e29d7a4ec6b0e36b82d4e942
Cleber Gasparoto <chgasparoto@gmail.com>
2019-02-26 21:40:24 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import axios from 'axios'
+import queryString from 'query-string'
+import 'dotenv/config'
+
+class Client {
+  constructor(token = null) {
+    this.token = token || process.env.CLASH_ROYALE_API_TOKEN
+  }
+
+  async request(route, query) {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: `${route}?${query ? queryString.stringify(query) : ''}`,
+        baseURL: 'https://api.clashroyale.com/v1/',
+        headers: {
+          Accept: 'application/json',
+          authorization: `Bearer ${this.token}`
+        }
+      })
+
+      return response.data
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
+
+  async cards() {
+    const response = await this.request('cards')
+    return response
+  }
+
+  async clans(params) {
+    let query = params
+
+    if (typeof params === 'string') {
+      query = { name: params }
+    }
+
+    const response = this.request('clans', query)
+    return response
+  }
+
+  async clan(tag, path) {
+    const response = await this.request(`clans/${encodeURIComponent(tag)}/${path ? path : ''}`)
+    return response
+  }
+
+  async locations(params = {}) {
+    const response = this.request('clans', params)
+    return response
+  }
+
+  async location(id, path) {
+    const response = await this.request(`clans/id/${path ? path : ''}`)
+    return response
+  }
+
+  async player(tag, path) {
+    const response = await this.request(`clans/${encodeURIComponent(tag)}/${path ? path : ''}`)
+    return response
+  }
+
+  async tournaments(params) {
+    let query = params
+
+    if (typeof params === 'string') {
+      query = { name: params }
+    }
+
+    const response = this.request('/tournaments', query)
+    return response
+  }
+
+  async tournament(tag) {
+    const response = this.request(`/tournaments/${encodeURIComponent(tag)}`)
+    return response
+  }
+
+  async globaltournaments() {
+    const response = await this.request('globaltournaments')
+    return response
+  }
+}
+
+export default Client
---
+++ src/test/index.spec.js
@@ -0,0 +1,22 @@
+import Client from '../index'
+
+const client = new Client()
+
+describe('Clash Royale API', () => {
+  test('should return cards data', async () => {
+    const response = await client.cards()
+    expect(response.items).toBeArray()
+    expect(response.items[0]).toBeObject()
+    expect(response.items[0]).toContainAllKeys(['name', 'id', 'maxLevel', 'iconUrls'])
+    expect(response.items[0].iconUrls).toBeObject()
+    expect(response.items[0].iconUrls).toContainAllKeys(['medium'])
+  })
+
+  test('should return clans data', async () => {
+    const response = await client.clans('hog')
+    expect(response.items).toBeArray()
+    expect(response.items[0]).toBeObject()
+    expect(response.items[0]).toContainAllKeys(["tag", "name", "type", "badgeId", "clanScore", "clanWarTrophies", "location", "requiredTrophies", "donationsPerWeek", "clanChestLevel", "clanChestMaxLevel", "members"])
+    expect(response.items[0].location).toBeObject()
+  })
+})
